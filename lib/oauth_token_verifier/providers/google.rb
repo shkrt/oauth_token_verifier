ede39532fa19cb005c1af18766998d76f2140b3b
Shkrt <zxcgpppmnn@gmail.com>
2017-11-22 18:27:11 +0300

    Fix rubocop offenses

--- lib/oauth_token_verifier/providers/google.rb
+++ lib/oauth_token_verifier/providers/google.rb
@@ -1,49 +1,51 @@
 # frozen_string_literal: true
 
-module OauthTokenVerifier::Providers
-  class Google
-    BaseFields = Struct.new(:uid, :provider, :info)
+module OauthTokenVerifier
+  module Providers
+    class Google
+      BaseFields = Struct.new(:uid, :provider, :info)
 
-    def initialize
-      @data_fields = Struct.new(*config.fields_mapping.values)
-    end
-
-    def verify_token(context)
-      uri = build_uri(context.token)
-      response = check_response(uri)
-      parse_response(response)
-    end
-
-    private
-
-    def config
-      OauthTokenVerifier.configuration.google
-    end
-
-    def build_uri(token)
-      params = { id_token: token }
-      URI::HTTPS.build(host: 'www.googleapis.com',
-                       path: '/oauth2/v3/tokeninfo',
-                       query: URI.encode_www_form(params))
-    end
-
-    def check_response(uri)
-      response = JSON.parse(Net::HTTP.get(uri))
-      if error = response['error_description']
-        raise OauthTokenVerifier::TokenVerifier::TokenCheckError, error
-      else
-        response
+      def initialize
+        @data_fields = Struct.new(*config.fields_mapping.values)
       end
-    end
 
-    def parse_response(data)
-      BaseFields.new(
-        data[config.id_field],
-        config.name,
-        @data_fields.new(
-          *data.values_at(*config.fields_mapping.keys.map(&:to_s))
+      def verify_token(context)
+        uri = build_uri(context.token)
+        response = check_response(uri)
+        parse_response(response)
+      end
+
+      private
+
+      def config
+        OauthTokenVerifier.configuration.google
+      end
+
+      def build_uri(token)
+        params = { id_token: token }
+        URI::HTTPS.build(host: 'www.googleapis.com',
+                         path: '/oauth2/v3/tokeninfo',
+                         query: URI.encode_www_form(params))
+      end
+
+      def check_response(uri)
+        response = JSON.parse(Net::HTTP.get(uri))
+        if error = response['error_description']
+          raise OauthTokenVerifier::TokenVerifier::TokenCheckError, error
+        else
+          response
+        end
+      end
+
+      def parse_response(data)
+        BaseFields.new(
+          data[config.id_field],
+          config.name,
+          @data_fields.new(
+            *data.values_at(*config.fields_mapping.keys.map(&:to_s))
+          )
         )
-      )
+      end
     end
   end
 end
--- lib/oauth_token_verifier/providers/vk.rb
+++ lib/oauth_token_verifier/providers/vk.rb
@@ -1,50 +1,52 @@
 # frozen_string_literal: true
 
-module OauthTokenVerifier::Providers
-  class Vk
-    BaseFields = Struct.new(:uid, :provider, :info)
+module OauthTokenVerifier
+  module Providers
+    class Vk
+      BaseFields = Struct.new(:uid, :provider, :info)
 
-    def initialize
-      @data_fields = Struct.new(*config.fields_mapping.values)
-      @request_fields = config.fields_mapping.keys.join(',')
-    end
-
-    def verify_token(context)
-      uri = build_uri(context.token)
-      response = check_response(uri)
-      parse_response(response)
-    end
-
-    private
-
-    def config
-      OauthTokenVerifier.configuration.vk
-    end
-
-    def build_uri(token)
-      params = { access_token: token, fields: @request_fields }
-      URI::HTTPS.build(host: 'api.vk.com',
-                       path: '/method/users.get',
-                       query: URI.encode_www_form(params))
-    end
-
-    def check_response(uri)
-      response = JSON.parse(Net::HTTP.get(uri))
-      if error = response['error']
-        raise OauthTokenVerifier::TokenVerifier::TokenCheckError, error['error_msg']
-      else
-        response['response'].first
+      def initialize
+        @data_fields = Struct.new(*config.fields_mapping.values)
+        @request_fields = config.fields_mapping.keys.join(',')
       end
-    end
 
-    def parse_response(data)
-      BaseFields.new(
-        data[config.id_field],
-        config.name,
-        @data_fields.new(
-          *data.values_at(*config.fields_mapping.keys.map(&:to_s))
+      def verify_token(context)
+        uri = build_uri(context.token)
+        response = check_response(uri)
+        parse_response(response)
+      end
+
+      private
+
+      def config
+        OauthTokenVerifier.configuration.vk
+      end
+
+      def build_uri(token)
+        params = { access_token: token, fields: @request_fields }
+        URI::HTTPS.build(host: 'api.vk.com',
+                         path: '/method/users.get',
+                         query: URI.encode_www_form(params))
+      end
+
+      def check_response(uri)
+        response = JSON.parse(Net::HTTP.get(uri))
+        if error = response['error']
+          raise OauthTokenVerifier::TokenVerifier::TokenCheckError, error['error_msg']
+        else
+          response['response'].first
+        end
+      end
+
+      def parse_response(data)
+        BaseFields.new(
+          data[config.id_field],
+          config.name,
+          @data_fields.new(
+            *data.values_at(*config.fields_mapping.keys.map(&:to_s))
+          )
         )
-      )
+      end
     end
   end
 end
