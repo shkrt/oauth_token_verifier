6eab52a66e6847c36ed388c0d67d3510b0c2fddd
Shkrt <zxcgpppmnn@gmail.com>
2017-11-23 19:57:26 +0300

    Add TokenVerifier successful configuration spec

--- spec/oauth_token_verifier_spec.rb
+++ spec/oauth_token_verifier_spec.rb
@@ -1,7 +1,26 @@
 require 'spec_helper'
+include ::OauthTokenVerifier
 
 RSpec.describe OauthTokenVerifier do
   it 'has a version number' do
     expect(OauthTokenVerifier::VERSION).not_to be nil
+  end
+end
+
+RSpec.describe TokenVerifier do
+  context 'when correctly configured' do
+
+    before(:each) do
+      OauthTokenVerifier.configure do |config|
+        config.enabled_providers = [:facebook]
+      end
+    end
+
+    it 'successfully initializes' do
+      verifier = described_class.new(:facebook, 'token')
+      expect(verifier).to be_instance_of(TokenVerifier)
+      expect(verifier.provider).to be_instance_of(OauthTokenVerifier::Providers::Facebook)
+      expect(verifier.token).to eq('token')
+    end
   end
 end
